7e8d20d2c6ec74508bcf202396b004b84154b1e9
Kollen Dillworth <kdillwor@sundevils.asu.edu>
2019-12-19 16:37:08 -0700

    bridal page formatting

--- src/app/bridal-page/bridal-page.component.ts
+++ src/app/bridal-page/bridal-page.component.ts
@@ -13,6 +13,12 @@ export class BridalPageComponent implements OnInit {
   kaylaIndex = 0;
   kristenIndex = 0;
   camiIndex = 0;
+  dillonIndex = 0;
+  bobIndex = 0;
+  erinIndex = 0;
+  xavierIndex = 0;
+  stephen1Index = 0;
+  stephen2Index = 0;
   constructor() { }
 
   ngOnInit() {
@@ -40,6 +46,30 @@ export class BridalPageComponent implements OnInit {
 
   onCamiInfoClick(index){
     this.camiIndex = index;
+  }
+
+  onDillonInfoClick(index){
+    this.dillonIndex = index;
+  }
+
+  onBobInfoClick(index){
+    this.bobIndex = index;
+  }
+
+  onErinInfoClick(index){
+    this.erinIndex = index;
+  }
+
+  onXavierInfoClick(index){
+    this.xavierIndex = index;
+  }
+
+  onStephen1InfoClick(index){
+    this.stephen1Index = index;
+  }
+
+  onStephen2InfoClick(index){
+    this.stephen2Index = index;
   }
 
 }
