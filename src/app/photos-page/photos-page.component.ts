9f79c19a57071712ff15e0c3942cf715f6b7e005
Kollen <kollen@Kollens-MacBook-Pro.local>
2019-12-31 08:03:46 -0800

    modal photo page on click

--- src/app/photos-page/photos-page.component.ts
+++ src/app/photos-page/photos-page.component.ts
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhotosPageComponent implements OnInit {
 
+
+  srcValue: string
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  expandImage(divName){
+//debugger;
+    this.srcValue = "/assets/images/natalie.jpg";
+
+    var modal = document.getElementById("myModal");
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var img = document.getElementById(divName);
+    
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+
+    modal.style.display = "block";
+    
+
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+ 
+    //modal.style.display = "none";
+
+
+  }
+
+  closeModal(){
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
   }
 
 }
