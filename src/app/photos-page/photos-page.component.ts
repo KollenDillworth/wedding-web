d8c24e7f70ee6e98d892b29739ed6acc5549cf7c
Kollen <kollen_281@hotmail.com>
2020-02-19 14:47:27 -0700

    Photos page functionality

--- src/app/photos-page/photos-page.component.ts
+++ src/app/photos-page/photos-page.component.ts
@@ -16,23 +16,16 @@ export class PhotosPageComponent implements OnInit {
   }
 
   expandImage(divName){
-//debugger;
-    this.srcValue = "/assets/images/natalie.jpg";
-
+    //debugger;
+     //this.srcValue = "/assets/images/natalie.jpg";
+this.srcValue = divName;
     var modal = document.getElementById("myModal");
-
     // Get the image and insert it inside the modal - use its "alt" text as a caption
-    var img = document.getElementById(divName);
-    
-    var modalImg = document.getElementById("img01");
-    var captionText = document.getElementById("caption");
 
     modal.style.display = "block";
-    
-
 
     // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+    //var span = document.getElementsByClassName("close")[0];
 
     // When the user clicks on <span> (x), close the modal
  
