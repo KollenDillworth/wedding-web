9ad2cf2b1eee04654bd162ea000e045fb9c847d4
Kollen Dillworth <kdillwor@sundevils.asu.edu>
2020-03-10 06:41:10 -0700

    countdown addition and styling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  
   constructor() { }
 
   ngOnInit() {
+    var countDownDate = new Date("Nov 7, 2020 17:00:00").getTime();
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    document.getElementById("demo").innerHTML ="See you in " + days + " days!";
+    var x = setInterval(function(){
+      now = new Date().getTime();
+      distance = countDownDate - now;
+
+      days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      document.getElementById("demo").innerHTML ="See you in " + days + " days!";
+
+      // if (distance < 0) {
+      //   clearInterval(x);
+      //   document.getElementById("demo").innerHTML = "EXPIRED";
+      // }
+    },60000);
   }
 
 }
